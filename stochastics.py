3eedd5aee27ee0dffb85d3740bad880ecf7e1c2d
Apostolos Kouzoukos <kouzoukos97@gmail.com>
2019-11-12 12:37:37 +0200

    Delete stochastics.py

--- stochastics.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import numpy as np
-
-def simulate_random_walk(N):
-	R = np.random.binomial(1, 0.5, N - 1)
-	R = np.array([-1 if i == 0 else i for i in R])
-
-	return np.concatenate((np.array([0]), R))
